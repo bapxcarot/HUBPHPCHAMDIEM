d74d8fb5bd18b29133ce46499901e4dfd489840b
ngocthuypv <ngocthuypv@gmail.com>
2019-03-15 12:10:26 +0700

    Về chúng tôi - update bảo mật
    <3

--- dulieu/do_register.php
+++ dulieu/do_register.php
@@ -1,16 +1,19 @@
 <?php
 	$usrname = $_POST['usrname'];
 	$name = $_POST['name'];
-	$password = md5($_POST['password']); 
+	$password = md5($_POST['password']);
 	$address = $_POST['address'];
 	$email = $_POST['email'];
 
-	require_once("mysql.php");  
+	$password = hash("sha384",sha1(md5(hash('ripemd160',md5($password))))).hash("ripemd1 60",sha1(md5(hash('sha512',md5($password)))));
+
+
+	require_once("mysql.php");
     $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
     mysqli_set_charset($conn, 'UTF8');
-    if ($conn->connect_error) 
-    { 
-        die("Không thể kết nối CSDL. Code: " . $conn->connect_error); 
+    if ($conn->connect_error)
+    {
+        die("Không thể kết nối CSDL. Code: " . $conn->connect_error);
     }
 
    	$sql_query = @mysql_query("SELECT * FROM nguoidung WHERE tendangnhap='{$usrname}'");
@@ -27,7 +30,7 @@
     	print "<script language='JavaScript'>alert('Email này không hợp lệ. Vui lòng nhập email khác!');</script>";
 		print "<meta http-equiv='refresh' content='0; index.php'>";
 		break;
-    }      
+    }
     //Kiểm tra email đã có người dùng chưa
     if (mysql_num_rows(mysql_query("SELECT email FROM nguoidung WHERE email='$email'")) > 0)
     {
